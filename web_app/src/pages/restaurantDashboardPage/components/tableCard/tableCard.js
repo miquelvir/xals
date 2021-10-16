46fb8c10e15c1e6980ecfa3169559ce4ed9d9a33
miquelvir <41950283+miquelvir@users.noreply.github.com>
2021-10-16 17:34:58 +0200

    #18 - make cards smaller

--- web_app/src/pages/restaurantDashboardPage/components/tableCard/tableCard.js
+++ web_app/src/pages/restaurantDashboardPage/components/tableCard/tableCard.js
@@ -8,15 +8,15 @@ import { userContext } from '../../../../contexts/userContext';
 const STATUS_ALARM = 'alarm';
 const STATUS_WARNING = 'warning';
 
-const useTheme = (status) => (status === STATUS_ALARM? {
-   bg:'bg-red-400 hover:bg-red-500',
+const useTheme = (status) => (status === STATUS_ALARM ? {
+   bg: 'bg-red-400 hover:bg-red-500',
    fg: 'bg-red-100',
    text: 'text-gray-800'
-}: status === STATUS_WARNING? {
+} : status === STATUS_WARNING ? {
    bg: 'bg-yellow-200 hover:bg-yellow-300',
    fg: 'bg-yellow-100',
    text: 'text-gray-800'
-}: {
+} : {
    bg: 'bg-green-200 hover:bg-green-300',
    fg: 'bg-green-50',
    text: 'text-gray-800'
@@ -43,30 +43,35 @@ function TableCard({
    }
 
    return <React.Fragment>
-      
-      {showActionMenu && <ActionMenu handleHide={handleHideActionMenu} handleShow={handleShowActionMenu} table={table}/>}
 
-       <BaseCard attributes={{[theme.bg]: true}} onClick={handleShowActionMenu} {...props}>
+      {showActionMenu && <ActionMenu handleHide={handleHideActionMenu} handleShow={handleShowActionMenu} table={table} />}
 
-      <div className={`flex space-x-4 ${theme.text}`}>
-         <div className="flex-2 flex flex-col">
-               <div className="flex-1"><p className="font-mono text-4xl">
-                  <StopWatch startTime={table.last_course_datetime} notifyDelta={handleStatus} precision='seconds'/>
-               </p></div>
+      <BaseCard attributes={{ [theme.bg]: true }} onClick={handleShowActionMenu} {...props}>
+
+         <div className={`flex-2 flex flex-col ${theme.text}`}>
+            <div className="flex-1">
+               <p className="font-mono text-4xl">
+                  <StopWatch startTime={table.last_course_datetime} notifyDelta={handleStatus} precision='seconds' />
+               </p>
+            </div>
+            <div className="flex space-x-4 pt-4" >
                <div className="text-left">
                   <span className={`inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold mr-2 mb-2 bottom-0 left-0 ${theme.fg}`}>
                      {table.next_course}
                   </span>
+               </div>
+               <div className="flex-1">
+                  <p className="font-mono text-right text-4xl">
+                     {table.number}
+                  </p>
+               </div>
             </div>
+         </div>
 
-         </div>
-         <div className="flex-1">
-            <p className="font-mono text-right text-8xl">
-               {table.number}
-            </p>
-         </div>
-      </div>
-   </BaseCard></React.Fragment>;
+
+
+
+      </BaseCard></React.Fragment>;
 }
 
 export default TableCard;
